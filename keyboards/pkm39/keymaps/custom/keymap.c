c3764b64fc4fd6eb147a6470ffcf4131fce73d95
Kota Takahashi <hoge@hoge.com>
2024-02-08 02:07:23 +0900

    かな送信込みのレイヤー切り替えがうまくいく例

--- keyboards/pkm39/keymaps/custom/keymap.c
+++ keyboards/pkm39/keymaps/custom/keymap.c
@@ -4,13 +4,13 @@
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes {
-    KANA_LAYER= SAFE_RANGE,
-    EISUU_LAYER,
-    VIM_ESC,
+    QWERTY = SAFE_RANGE,
+    KANA_LAYER,
+    // EISUU_LAYER,
+    // VIM_ESC,
 };
 
 bool is_kana_key_active = false;
-bool is_eisuu_key_active = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -35,35 +35,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 is_kana_key_active = false;
             }
             break;
-        case EISUU_LAYER:
-            if (record->event.pressed) {
-                if (record->tap.count > 0) {
-                    if (!is_eisuu_key_active) {
-                        register_code(KC_LNG2);
-                        is_eisuu_key_active = true;
-                    }
-                } else {
-                    // キーを長押ししている間の処理（かなキーを入力してレイヤーを切り替える）
-                    register_code(KC_LNG2); 
-                    layer_on(2); // レイヤー2をアクティベート
-                    is_eisuu_key_active = true;
-                }
-            } else {
-                // キーが離されたときの処理
-                unregister_code(KC_LNG2);
-                layer_off(2);
-                is_eisuu_key_active = false;
-            }
-            break;
-        case VIM_ESC:
-            if (record->event.pressed) {
-                register_code(KC_LNG2);
-                register_code(KC_ESC);
-                unregister_code(KC_ESC);
-                unregister_code(KC_LNG2);
-            }
-            break;
-            return false;
     }
     return true;
 }
@@ -83,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT(3, KC_ENT),
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,    KC_DOT,    KC_LEFT_BRACKET,
-        VIM_ESC,    KC_LALT,    LCTL_T(KC_BSPC),    EISUU_LAYER,    LSFT_T(KC_SPACE),  KANA_LAYER,     LGUI_T(KC_DEL), QK_BOOT,  KC_RIGHT_BRACKET
+        KC_ESC,    KC_LALT,    LCTL_T(KC_BSPC),    LT(2, KC_LNG2),    LSFT_T(KC_SPACE),  KANA_LAYER,     LGUI_T(KC_DEL), QK_BOOT,  KC_RIGHT_BRACKET
     ),
     [1] = LAYOUT_ortho_4x10(
         LSFT(KC_1),    LSFT(KC_2),    LSFT(KC_3),    LSFT(KC_4),    LSFT(KC_5),    LSFT(KC_6),    LSFT(KC_7),    LSFT(KC_8),    LSFT(KC_9),    LSFT(KC_0),
