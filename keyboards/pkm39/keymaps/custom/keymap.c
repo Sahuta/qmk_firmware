63110d17686d5046b5a779a94d07a87b86236d74
Kota Takahashi <hoge@hoge.com>
2024-02-09 02:18:57 +0900

    EEPROMクリアは大事。

--- keyboards/pkm39/keymaps/custom/keymap.c
+++ keyboards/pkm39/keymaps/custom/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F11,    KC_F12,    KC_F13,    KC_F14,    KC_F15,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT,    KC_ENT,
         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_PRINT_SCREEN,    RCS(KC_P),    LGUI(KC_TAB),    KC_TAB,    KC_0,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        QK_CLEAR_EEPROM, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
 
