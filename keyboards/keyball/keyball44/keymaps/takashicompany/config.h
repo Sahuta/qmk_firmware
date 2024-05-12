9a19ea668f91bf178f8a3ecc8cf2d2d484bb2610
Kota Takahashi <hoge@hoge.com>
2024-05-12 22:52:36 +0900

    keyball 調整

--- keyboards/keyball/keyball44/keymaps/takashicompany/config.h
+++ keyboards/keyball/keyball44/keymaps/takashicompany/config.h
@@ -21,9 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//#    define RGBLIGHT_EFFECT_BREATHING
+//#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 //#    define RGBLIGHT_EFFECT_SNAKE
 //#    define RGBLIGHT_EFFECT_KNIGHT
 //#    define RGBLIGHT_EFFECT_CHRISTMAS
@@ -34,4 +34,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #define TAP_CODE_DELAY 5
+
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+
+#define AUTO_MOUSE_DEFAULT_LAYER 6 // 切り替えるマウスレイヤー番号を指定
+#define AUTO_MOUSE_TIME 500 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
